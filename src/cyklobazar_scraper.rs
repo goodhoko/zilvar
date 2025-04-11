73a2e7cde047077ead91f6a395bf615c677ae6a5
Jen Tak <goodhoko@gmail.com>
2025-04-11 16:40:49 +0200

    Run  doggos in an infinite loop in memory

---
+++ src/cyklobazar_scraper.rs
@@ -0,0 +1,49 @@
+use crate::Ad;
+use eyre::{Context, Result};
+use once_cell::sync::Lazy;
+use regex::Regex;
+use reqwest::{Url, get};
+use scraper::{ElementRef, Html, Selector};
+
+pub async fn get_ads(url: Url) -> Result<Vec<Ad>> {
+    let response = get(url.clone())
+        .await
+        .with_context(|| format!("fetching {url}"))?;
+    let response_body = response
+        .text()
+        .await
+        .with_context(|| format!("decoding response body from {url}"))?;
+    let html = Html::parse_document(&response_body);
+
+    let ad_selector = Selector::parse(
+        ".content-layout__main
+        > ul.cb-offer-list:not(.cb-offer-list--vertical)
+        > li.cb-offer-list__item
+        > a.cb-offer:not(.cb-offer--ad)",
+    )
+    .expect("valid selector");
+    let ads = html
+        .select(&ad_selector)
+        .filter_map(|item| {
+            Some(Ad {
+                id: parse_id(item)?.to_string(),
+                title: parse_title(item)?,
+            })
+        })
+        .collect();
+
+    Ok(ads)
+}
+
+fn parse_id(ad: ElementRef) -> Option<&str> {
+    let href = ad.attr("href")?;
+    static RE: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"/inzerat/(?P<id>.+?)/").expect("valid regex"));
+    Some(RE.captures(href)?.name("id")?.as_str())
+}
+
+fn parse_title(ad: ElementRef) -> Option<String> {
+    let heading_selector = Selector::parse("div.cb-offer__header h4").expect("valid selector");
+    let text_nodes = ad.select(&heading_selector).next()?.text();
+    Some(text_nodes.collect::<Vec<_>>().join(" "))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,135 @@
-use eyre::{Context, Result};
-use futures::StreamExt;
-use once_cell::sync::Lazy;
-use regex::Regex;
-use reqwest::get;
-use scraper::{ElementRef, Html, Selector};
-use sqlx::{Executor, Pool, Sqlite, migrate::MigrateDatabase, sqlite::SqlitePoolOptions};
+use std::{collections::HashMap, time::Duration};
+
+use cyklobazar_scraper::get_ads;
+use db::setup_db;
+use eyre::Result;
+use jiff::{SignedDuration, Timestamp};
+use reqwest::Url;
+use tokio::time::sleep;
+
+mod cyklobazar_scraper;
+mod db;
+
+const SCRAPING_INTERVAL: Duration = Duration::from_secs(60 * 10);
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let url = "https://www.cyklobazar.cz/sedlovky?filter%5Blist_types_id%5D=15";
+    // TODO: fetch from DB every iteration instead.
+    let mut doggos = vec![Doggo::new(
+        "jentak@hey.com".to_string(),
+        "https://www.cyklobazar.cz/sedlovky?filter%5Blist_types_id%5D=15"
+            .parse()
+            .expect("valid url"),
+    )];
 
-    let ads = get_ads(url)
-        .await
-        .with_context(|| format!("fetching ads from {url}"))?;
-    println!("{ads:#?}");
+    let _db = setup_db().await?;
 
-    let db = setup_db().await?;
+    loop {
+        let pending_doggos = doggos.iter_mut().filter(|doggo| doggo.should_run_now());
 
-    let new_ads = futures::stream::iter(ads.iter())
-        .filter(|ad| async {
-            let query = sqlx::query("INSERT INTO ads ( id, title ) VALUES ($1, $2);")
-                .bind(&ad.id)
-                .bind(&ad.title);
+        for doggo in pending_doggos {
+            if let Err(err) = doggo.run().await {
+                println!("Failed to run {doggo:?}: {err:#}");
+            }
+        }
 
-            let Ok(res) = db.execute(query).await else {
-                return false;
-            };
+        let mut next_runs: Vec<Timestamp> = doggos.iter().map(|doggo| doggo.next_run()).collect();
+        next_runs.sort();
+        let closest_next_run = next_runs.first();
+        let until_next_run = closest_next_run
+            .map(|next| Timestamp::now().duration_until(*next))
+            // Prevent busy loops if there are no doggos whatsoever.
+            .unwrap_or(SignedDuration::from_secs(1))
+            // Prevent busy loops, just in case.
+            .max(SignedDuration::from_secs(1))
+            // We're already positive thanks to max() above but we need to convert the type to
+            // Duration to be accepted by sleep().
+            .unsigned_abs();
 
-            res.rows_affected() == 1
-        })
-        .collect::<Vec<_>>()
-        .await;
+        println!(
+            "Sleeping {until_next_run:?} until the next closest run at {:?}",
+            closest_next_run
+        );
 
-    println!("NEW: {new_ads:#?}");
-
-    Ok(())
-}
-
-async fn setup_db() -> Result<Pool<Sqlite>> {
-    let db_url = "./sqlite:db";
-
-    if !Sqlite::database_exists(db_url).await? {
-        Sqlite::create_database(db_url).await?;
+        sleep(until_next_run).await
     }
-
-    let pool = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect(db_url)
-        .await?;
-
-    let query = sqlx::query("CREATE TABLE IF NOT EXISTS ads (title TEXT, id TEXT PRIMARY KEY);");
-    pool.execute(query).await.context("creating ads table")?;
-
-    Ok(pool)
-}
-
-async fn get_ads(url: &str) -> Result<Vec<Ad>> {
-    let response = get(url).await.with_context(|| format!("fetching {url}"))?;
-    let response_body = response
-        .text()
-        .await
-        .with_context(|| format!("decoding response body from {url}"))?;
-    let html = Html::parse_document(&response_body);
-
-    // TODO: filter out topped ads - they're unrelated junk mostly.
-    let ad_selector = Selector::parse("ul.cb-offer-list li.cb-offer-list__item a.cb-offer")
-        .expect("valid selector");
-    let ads = html
-        .select(&ad_selector)
-        .filter_map(|item| {
-            Some(Ad {
-                id: parse_id(item)?.to_string(),
-                title: parse_title(item)?,
-            })
-        })
-        .collect();
-
-    Ok(ads)
-}
-
-fn parse_id(ad: ElementRef) -> Option<&str> {
-    let href = ad.attr("href")?;
-    static RE: Lazy<Regex> =
-        Lazy::new(|| Regex::new(r"/inzerat/(?P<id>.+?)/").expect("valid regex"));
-    Some(RE.captures(href)?.name("id")?.as_str())
-}
-
-fn parse_title(ad: ElementRef) -> Option<String> {
-    let heading_selector = Selector::parse("div.cb-offer__header h4").expect("valid selector");
-    let text_nodes = ad.select(&heading_selector).next()?.text();
-    Some(text_nodes.collect::<Vec<_>>().join(" "))
 }
 
 #[derive(Debug, Clone)]
 struct Ad {
     title: String,
     id: String,
+}
+
+#[derive(Debug, Clone)]
+struct Doggo {
+    email: String,
+    url: Url,
+    last_run: Option<Timestamp>,
+    seen_ads: HashMap<String, Ad>,
+}
+
+impl Doggo {
+    pub fn new(email: String, url: Url) -> Self {
+        Self {
+            email,
+            url,
+            last_run: None,
+            seen_ads: HashMap::new(),
+        }
+    }
+
+    pub fn should_run_now(&self) -> bool {
+        self.last_run.is_none_or(|las_run| {
+            Timestamp::now().duration_since(las_run)
+                > SCRAPING_INTERVAL
+                    .try_into()
+                    .expect("SCRAPING_INTERVAL fits into Duration")
+        })
+    }
+
+    pub fn next_run(&self) -> Timestamp {
+        let Some(last_run) = self.last_run else {
+            return Timestamp::now();
+        };
+
+        last_run + SCRAPING_INTERVAL
+    }
+
+    pub async fn run(&mut self) -> Result<()> {
+        let mut new_ads = get_ads(self.url.clone()).await?;
+        new_ads.retain(|ad| {
+            // TODO: changed price should behave as if the ad is new.
+            !self.seen_ad(ad)
+        });
+
+        println!(
+            "Found {} new ads for {} ({}):",
+            new_ads.len(),
+            self.email,
+            self.url
+        );
+
+        // TODO: notify email instead
+        println!("{new_ads:#?}");
+
+        for ad in new_ads.iter() {
+            self.see_ad(ad.clone());
+        }
+        self.last_run = Some(Timestamp::now());
+
+        Ok(())
+    }
+
+    fn seen_ad(&self, ad: &Ad) -> bool {
+        // TODO: ask DB
+        self.seen_ads.contains_key(&ad.id)
+    }
+
+    fn see_ad(&mut self, ad: Ad) {
+        // TODO: persist to DB
+        self.seen_ads.insert(ad.id.clone(), ad);
+    }
 }
